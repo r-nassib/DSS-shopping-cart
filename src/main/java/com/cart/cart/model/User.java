d0cfa0b76f3207f75f2c90e0c5848094a85b18ec
Raul Hidalgo <rhidalgou2@gmail.com>
2025-12-16 11:51:33 +0100

    Ajustes

--- src/main/java/com/cart/cart/model/User.java
+++ src/main/java/com/cart/cart/model/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +19,7 @@ import lombok.Setter;
 import java.util.Set;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
